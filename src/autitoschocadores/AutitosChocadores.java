e7de9f6a75a76a7e9e92eb7355e5be7901db2b55
coderpaco <bob.theerbes@gmail.com>
2024-04-23 17:05:59 -0500

    lil fix w syntax and ranking
    added the missing break in switch case causing ranking to print when you made the board

--- src/autitoschocadores/AutitosChocadores.java
+++ src/autitoschocadores/AutitosChocadores.java
@@ -3,13 +3,16 @@ package autitoschocadores;
 import java.util.*;
 // version:4/23/2024 @ 6:53
 
-public class AutitosChocadores {
+public class AutitosChocadores 
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         AutitosChocadores game = new AutitosChocadores();// select option (al azar, propio, predefinido)
         game.initializeGameBoard();
     }
-    public AutitosChocadores() {
+    public AutitosChocadores() 
+    {
         initializeGameBoard();
     }
 
@@ -28,13 +31,15 @@ public class AutitosChocadores {
 
    
 
-    private void initializeGameBoard() {
+    private void initializeGameBoard() 
+    {
         Ranking ranking = new Ranking(new ArrayList<>());
         board = new Board(m); 
         System.out.println("Bienvenidos! Te gustaria jugar Autitos Chocadores? (si/no)");
         String choice = scanner.nextLine().toLowerCase();
         
-        while (choice.equals("si")) {
+        while (choice.equals("si")) 
+        {
             // Ask the player for the game setup option
             System.out.println("Eligir un opcion:");
             System.out.println("a) Registrar un jugador");
@@ -45,7 +50,8 @@ public class AutitosChocadores {
 
             char setupOption = scanner.nextLine().toLowerCase().charAt(0);
 
-            switch (setupOption) {
+            switch (setupOption) 
+            {
                 case 'a':// user input to create table
                     ranking.addPlayer(createPlayer());
                     break;
@@ -84,6 +90,7 @@ public class AutitosChocadores {
                             System.out.println("Opcion invalido, saliendo del juego.");
                             System.exit(0);
                     }
+                    break;
                 case 'd':
                     ranking.sortByPoints();
                     ranking.displayRanking(); // Display the sorted ranking
@@ -100,7 +107,8 @@ public class AutitosChocadores {
         }
     }
 
-    private void playPredefinedGame() {
+    private void playPredefinedGame() 
+    {
         m = 5;
        
        // board = new char[m][m]; // Initialize the game board IM INITIALIZING UP
@@ -118,8 +126,10 @@ public class AutitosChocadores {
         displayGameBoard(); // Display the game board with predefined cars
     }
 
-    private void placeCar(String input) {
-        try {
+    private void placeCar(String input) 
+    {
+        try 
+        {
             String[] parts = input.split(" ");
             String position = parts[0];
             int direction = Integer.parseInt(parts[1]);
@@ -128,49 +138,61 @@ public class AutitosChocadores {
             int col = Integer.parseInt(position.substring(1)) - 1;
 
             // Check if the calculated indices are within the bounds of the board
-            if (row >= 0 && row < m && col >= 0 && col < m) {
+            if (row >= 0 && row < m && col >= 0 && col < m) 
+            {
             // Create a new Autito object with calculated position and set orientation
-            Autito newCar = new Autito(row * m + col);  // Convert row and column to a single index (assuming linear board representation)
-            newCar.setOrientation(direction);  // Set the orientation based on user input
-            board.placeAutito(row, col, newCar); // Place the Autito object on the board
-
-            } else {
+                Autito newCar = new Autito(row * m + col);  // Convert row and column to a single index (assuming linear board representation)
+                newCar.setOrientation(direction);  // Set the orientation based on user input
+                board.placeAutito(row, col, newCar); // Place the Autito object on the board
+            } else 
+            {
                 System.out.println("Posición fuera de límites, intente de nuevo.");
             }
-        } catch (Exception e) {
+        } catch (Exception e) 
+        {
             System.out.println("Error en la entrada. Formato correcto: A12 2");
         }
     }
 
-    private void displayGameBoard() {
+    private void displayGameBoard() 
+    {
         printColumnNumbers(); // Print column numbers at the top
     
-        for (int row = 0; row < m; row++) {
+        for (int row = 0; row < m; row++) 
+        {
             printRowBorder();
 
-            for (int i = 0; i < 4; i++) { // Loop for each row within a cell
+            for (int i = 0; i < 4; i++) 
+            { // Loop for each row within a cell
                 // Print the row border
-                if (i == 0) {
+                if (i == 0) 
+                {
                     System.out.print((char) ('A' + row)); // Print the row LETTER (a,b,c,d) only for the first cell in a row
                 }
-                if(i != 0){
+                if(i != 0)
+                {
                     System.out.print((char) (' ')); // Print the row LETTER (a,b,c,d) only for the first cell in a row
                 }
                 System.out.print(" |");
     
-                for (int col = 0; col < m; col++) {
+                for (int col = 0; col < m; col++) 
+                {
                     Autito autito = board.getAutitoAt(row, col);
                     
-                    if (autito != null) {
+                    if (autito != null) 
+                    {
                         char[][] carRepresentation = autito.getCurrentOrientation();
-                        for (int j = 0; j < 1; j++) { // Loop for each row in a car representation
-                            for (int k = 0; k < 4; k++) { // Loop for each column in a row of car representation
+                        for (int j = 0; j < 1; j++) 
+                        { // Loop for each row in a car representation
+                            for (int k = 0; k < 4; k++) 
+                            { // Loop for each column in a row of car representation
                                 System.out.print(carRepresentation[i][k]);
                             }
                             // Add whitespace padding to make each cell the same size
                             System.out.print("");
                         }
-                    } else {
+                    } else 
+                    {
                         // No cars, print empty spaces
                         System.out.print("    ");
                     }
@@ -184,18 +206,22 @@ public class AutitosChocadores {
         System.out.println(); // Add an extra line for clarity
     }
     
-    public int getValidInput(int min, int max) {
+    public int getValidInput(int min, int max) 
+    {
         int input = 0;
         while (input == 0) {
-            try {
+            try 
+            {
                 int value = scanner.nextInt();
                 scanner.nextLine();
                 if (value >= min && value <= max) {
                     input = value;
-                } else {
+                } else 
+                {
                     System.out.println("Error: Input out of bounds");
                 }
-            } catch (InputMismatchException e) {
+            } catch (InputMismatchException e) 
+            {
                 System.out.println("Error: Invalid Input (InputMismatchException)");
                 scanner.nextLine();
             }
@@ -203,23 +229,28 @@ public class AutitosChocadores {
         return input;
     }
 
-    private void printColumnNumbers() {
+    private void printColumnNumbers() 
+    {
         System.out.print(" "); // whitespace before nums
-        for (int j = 0; j < m; j++) {
+        for (int j = 0; j < m; j++) 
+        {
             System.out.printf("%5d", (j + 1)); // pads nums with spaces tomake them at least 4 chars wide
         }
         System.out.println();
     }
 
-    private void printRowBorder() {
+    private void printRowBorder() 
+    {
         System.out.print("  +"); // beginning
-        for (int j = 0; j < m; j++) {
+        for (int j = 0; j < m; j++) 
+        {
             System.out.print("----+"); // car can be 2x4 or 4x2 spaces, should make each cell 4 wide
         }
         System.out.println();
     }
 
-public Jugadores createPlayer() {
+public Jugadores createPlayer() 
+{
     System.out.println("Ingrese su nombre:");
     String nameInput = scanner.nextLine();
     System.out.println("Ingrese su edad: (1-100)");
