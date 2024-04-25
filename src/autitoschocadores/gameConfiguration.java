ae15e06b282e378185c74e169146a232d066dc86
diegoppuig <diego.puig27@gmail.com>
2024-04-24 21:41:09 -0300

    Cambios

---
+++ src/autitoschocadores/gameConfiguration.java
@@ -0,0 +1,28 @@
+package autitoschocadores;
+
+public class gameConfiguration {
+    public class GameConfiguration {
+        private int boardSize;
+        private int numCars;
+        private String[] carPositions;
+    
+        public GameConfiguration(int boardSize, int numCars, String[] carPositions) {
+            this.boardSize = boardSize;
+            this.numCars = numCars;
+            this.carPositions = carPositions;
+        }
+    
+        public int getBoardSize() {
+            return boardSize;
+        }
+    
+        public int getNumCars() {
+            return numCars;
+        }
+    
+        public String[] getCarPositions() {
+            return carPositions;
+        }
+    }
+}
+
